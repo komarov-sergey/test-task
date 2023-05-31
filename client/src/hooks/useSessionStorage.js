2d40488f10f556b842fe07fd110cbb1ccd76c5c9
Sergey Komarov <komarovs33@mail.ru>
2023-05-31 18:15:13 +0300

    fix: linter

--- client/src/hooks/useSessionStorage.js
+++ client/src/hooks/useSessionStorage.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react'
 
+// eslint-disable-next-line
 export default (key, initialValue = '') => {
   const [value, setValue] = useState(() => {
     return sessionStorage.getItem(key) || initialValue
